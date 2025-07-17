840bca66b084b1d255c5f7ae2f1c00e44548e952
José <jcuencacoronado@gmail.com>
2025-07-17 17:31:28 -0400

    AuthController comentado para usar otro microservicio de auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,34 +5,34 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('auth')
 export class AuthController {
-  private readonly logger = new Logger('AuthController');
-  constructor(private readonly authService: AuthService) {}
+  // private readonly logger = new Logger('AuthController');
+  // constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LdapAuthGuard)
-  @MessagePattern('auth.login')
-  async login(@Payload() data: any) {
-    const jwt = await this.authService.generateJwt(data.user);
-    return {
-      access_token: jwt,
-      user: {
-        username: data.user.uid,
-        name: data.user.cn,
-      },
-    };
-  }
+  // @UseGuards(LdapAuthGuard)
+  // @MessagePattern('auth.login')
+  // async login(@Payload() data: any) {
+  //   const jwt = await this.authService.generateJwt(data.user);
+  //   return {
+  //     access_token: jwt,
+  //     user: {
+  //       username: data.user.uid,
+  //       name: data.user.cn,
+  //     },
+  //   };
+  // }
 
-  @MessagePattern('auth.verify.token')
-  async verifyToken(@Payload() token: string) {
-    this.logger.debug('verify token');
-    const username = await this.authService.verifyToken(token);
-    return {
-      username,
-    };
-  }
+  // @MessagePattern('auth.verify.token')
+  // async verifyToken(@Payload() token: string) {
+  //   this.logger.debug('verify token');
+  //   const username = await this.authService.verifyToken(token);
+  //   return {
+  //     username,
+  //   };
+  // }
 
-  @MessagePattern('auth.verify.apikey')
-  async verifyApiKey(@Payload() apikey: string) {
-    this.logger.debug('verify apikey');
-    return await this.authService.verifyApiKey(apikey);
-  }
+  // @MessagePattern('auth.verify.apikey')
+  // async verifyApiKey(@Payload() apikey: string) {
+  //   this.logger.debug('verify apikey');
+  //   return await this.authService.verifyApiKey(apikey);
+  // }
 }
